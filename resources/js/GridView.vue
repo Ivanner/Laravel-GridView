b70ec5e9b42a89d0dc3795fd62f70fa483e7430b
Ivanner Mora <ivanner@gmail.com>
2021-07-06 07:45:30 -0500

    Send boolean value

--- resources/js/GridView.vue
+++ resources/js/GridView.vue
@@ -1,83 +1,84 @@
 <script>
-    export default {
-        props: {
-            id: String,
-            originFilters: Object,
-            sortBy: String,
-            sortOrder: String,
-            ajaxUpdate: Boolean,
-            targetUrl: String,
-        },
+export default {
+  props: {
+    id: String,
+    originFilters: Object,
+    sortBy: String,
+    sortOrder: String,
+    ajaxUpdate: String,
+    targetUrl: String,
+  },
 
-        data() {
-            return {
-                filters: Object.assign({}, this.originFilters),
-                sortDesc: this.sortOrder === 'DESC',
-                sortColumn: this.sortBy,
-                filterTimeout: null,
-            }
-        },
+  data() {
+    return {
+      filters: Object.assign({}, this.originFilters),
+      sortDesc: this.sortOrder === 'DESC',
+      useAjax: this.ajaxUpdate === 'true' || parseInt(this.ajaxUpdate) === 1,
+      sortColumn: this.sortBy,
+      filterTimeout: null,
+    }
+  },
 
-        methods: {
-            filter(skipDelay = false) {
+  methods: {
+    filter(skipDelay = false) {
 
-                if (skipDelay) {
-                    this.$nextTick(() => {
-                        this.sendForm();
-                    });
-                    return;
-                }
+      if (skipDelay) {
+        this.$nextTick(() => {
+          this.sendForm();
+        });
+        return;
+      }
 
-                if (this.filterTimeout) {
-                    clearTimeout(this.filterTimeout);
-                }
+      if (this.filterTimeout) {
+        clearTimeout(this.filterTimeout);
+      }
 
-                this.filterTimeout = setTimeout(() => {
-                    this.sendForm();
-                }, 1000);
-            },
+      this.filterTimeout = setTimeout(() => {
+        this.sendForm();
+      }, 1000);
+    },
 
-            sort(column) {
-                this.sortColumn = column;
-                this.sortDesc = !this.sortDesc;
+    sort(column) {
+      this.sortColumn = column;
+      this.sortDesc = !this.sortDesc;
 
-                this.$nextTick(() => {
-                    this.sendForm();
-                });
-            },
+      this.$nextTick(() => {
+        this.sendForm();
+      });
+    },
 
-            sendForm() {
-              if (!this.ajaxUpdate) {
-                this.$refs.gridForm.submit();
-              } else {
-                let formData = new FormData(this.$refs.gridForm);
-                fetch(this.targetUrl, formData)
-                    .then(function (data) {
-                      this.dataset = data;
-                    })
-              }
+    sendForm() {
+      if (!this.useAjax) {
+        this.$refs.gridForm.submit();
+      } else {
+        let formData = new FormData(this.$refs.gridForm);
+        fetch(this.targetUrl, formData)
+            .then(function (data) {
+              this.dataset = data;
+            })
+      }
 
-            }
-        }
-    };
+    }
+  }
+};
 </script>
 <style lang="scss">
-    .sort-asc, .sort-desc {
-        display: inline-block;
-        width: 0;
-        height: 0;
-        margin-bottom: 5px;
+.sort-asc, .sort-desc {
+  display: inline-block;
+  width: 0;
+  height: 0;
+  margin-bottom: 5px;
 
-        &.sort-asc {
-            border-left: 5px solid transparent;
-            border-right: 5px solid transparent;
-            border-bottom: 5px solid #007bff;
-        }
+  &.sort-asc {
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-bottom: 5px solid #007bff;
+  }
 
-        &.sort-desc {
-            border-left: 5px solid transparent;
-            border-right: 5px solid transparent;
-            border-top: 5px solid #007bff;
-        }
-    }
+  &.sort-desc {
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 5px solid #007bff;
+  }
+}
 </style>
